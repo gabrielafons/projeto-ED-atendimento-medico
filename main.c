337bd1b9a2040ba2b29e45fb7a47a06af7fe96a8
Gabriel Afonso dos Santos <141970054+gabrielafons@users.noreply.github.com>
2025-04-19 22:35:59 +0000

    inicio projeto

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct Paciente{
+    char nome;
+    int idade;
+    char rg;
+    // Data entrada;
+}Paciente;
+
+// typedef struct Data{
+//     int dia;
+//     int mes;
+//     int ano;
+// }Data;
+
+
+
+
+
+int main(){
+    printf("1 - Cadastrar novo paciente");
+    printf("\n");
+    printf("2 - Atendimento");
+    
+}
